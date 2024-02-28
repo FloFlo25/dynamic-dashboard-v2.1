776a88fb5643e35deb700d6407b902c313abc84a
Raul Floruta <floruta.raul25@gmail.com>
2024-02-28 17:50:24 +0200

    update

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+	output: "export",
+	reactStrictMode: true,
+	basePath: "/dynamic-dashboard-v2.1",
+};
 
 export default config;
