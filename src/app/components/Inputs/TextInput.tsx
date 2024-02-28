babb8ace9efe0028259a72360b060ca547e5232e
Raul Floruta <floruta.raul25@gmail.com>
2024-02-28 14:22:54 +0200

    Prettier config updated and made some progress on the TextInput base component

--- src/app/components/Inputs/TextInput.tsx
+++ src/app/components/Inputs/TextInput.tsx
@@ -1,8 +1,73 @@
-import { TextField, TextFieldProps } from "@mui/material";
+import {
+	IconButton,
+	InputAdornment,
+	TextField,
+	type TextFieldProps,
+} from "@mui/material";
 import React from "react";
 
-const TextInput = (props: TextFieldProps) => {
-  return <TextField {...props} sx={{ borderRadius: "20px", border: "red" }} />;
+type Props = {
+	startIcon?: React.ReactNode;
+	endIcon?: React.ReactNode;
+	onEndButtonClick?: (() => void) | undefined;
+} & TextFieldProps;
+
+const TextInput = ({
+	startIcon,
+	endIcon,
+	onEndButtonClick,
+	...props
+}: Props) => {
+	return (
+		<TextField
+			{...props}
+			label=""
+			placeholder={props.placeholder ?? "Placeholder"}
+			InputProps={{
+				startAdornment: (
+					<InputAdornment position="start">
+						{startIcon}
+					</InputAdornment>
+				),
+				endAdornment: (
+					<InputAdornment position="end">
+						{onEndButtonClick ? (
+							<IconButton
+								onClick={
+									onEndButtonClick
+								}
+							>
+								{
+									endIcon
+								}
+							</IconButton>
+						) : (
+							endIcon
+						)}
+					</InputAdornment>
+				),
+			}}
+			InputLabelProps={{
+				shrink: false,
+			}}
+			sx={{
+				"& .MuiOutlinedInput-root": {
+					"& fieldset": {
+						borderRadius: "14px",
+						borderWidth: "2px",
+					},
+					"&.Mui-focused fieldset": {
+						borderColor: "black",
+					},
+					"& legend span": {
+						color: "red",
+					},
+				},
+				"& .MuiInputAdornment-positionStart svg":
+					{ color: "black" },
+			}}
+		/>
+	);
 };
 
 export default TextInput;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,50 @@
+"use client";
+
+import AlternateEmailRoundedIcon from "@mui/icons-material/AlternateEmailRounded";
+import PasswordRoundedIcon from "@mui/icons-material/PasswordRounded";
+import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
 import React from "react";
+
 import TextInput from "../components/Inputs/TextInput";
 
 const Login = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-      <div className="flex flex-col">
-        <TextInput label="Email" id="email" />
-        <TextInput label="Password" id="password" />
-      </div>
-    </div>
-  );
+	const [showPassword, setShowPassword] = React.useState(false);
+
+	const onClickShowPassword = () =>
+		setShowPassword((prevState) => !prevState);
+
+	return (
+		<div>
+			<h1>Login</h1>
+			<div className="flex flex-col p-5 gap-4">
+				<TextInput
+					placeholder="Email"
+					id="email"
+					startIcon={
+						<AlternateEmailRoundedIcon />
+					}
+				/>
+				<TextInput
+					placeholder="Password"
+					id="password"
+					startIcon={
+						<PasswordRoundedIcon />
+					}
+					endIcon={
+						<VisibilityRoundedIcon />
+					}
+					onEndButtonClick={
+						onClickShowPassword
+					}
+					type={
+						showPassword
+							? "text"
+							: "password"
+					}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
