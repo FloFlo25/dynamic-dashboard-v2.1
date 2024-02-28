074f0ea0f5fadde404e3ea1efd1437b941044554
Raul Floruta <floruta.raul25@gmail.com>
2024-02-28 14:45:20 +0200

    added logo and put bg image

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,6 +6,7 @@ import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
 import React from "react";
 
 import TextInput from "../components/Inputs/TextInput";
+import Image from "next/image";
 
 const Login = () => {
 	const [showPassword, setShowPassword] = React.useState(false);
@@ -14,8 +15,18 @@ const Login = () => {
 		setShowPassword((prevState) => !prevState);
 
 	return (
-		<div>
-			<h1>Login</h1>
+		<div
+			className="flex flex-col items-center gap-6 bg-cover bg-center h-full"
+			style={{
+				backgroundImage: `url("https://dynamicstudio.ro/wp-content/uploads/2022/11/Burning-Cardio.jpg")`,
+			}}
+		>
+			<Image
+				alt="Dynamic Logo"
+				width={380}
+				height={60}
+				src="dynamicLogo.svg"
+			/>
 			<div className="flex flex-col p-5 gap-4">
 				<TextInput
 					placeholder="Email"
