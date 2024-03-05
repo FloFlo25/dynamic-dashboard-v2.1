3982a7fd25ddc7b994435d94ed5f5dfc95b22cf5
Raul Floruta <floruta.raul25@gmail.com>
2024-03-05 12:08:52 +0200

    update

--- src/app/(authentification)/test/page.tsx
+++ src/app/(authentification)/test/page.tsx
@@ -3,12 +3,6 @@
 import React from "react";
 
 const LegacyPage = () => {
-	const access_token = localStorage.getItem("access_token")!;
-	const refresh_token = localStorage.getItem("refresh_token")!;
-
-	console.log(access_token);
-	console.log(refresh_token);
-
 	return (
 		<iframe
 			src="https://test.dynamicapp.ro"
@@ -16,16 +10,6 @@ const LegacyPage = () => {
 			width="100%"
 			height="100%"
 			allowFullScreen
-			onEnded={() => {
-				localStorage.setItem(
-					"access_token",
-					access_token,
-				);
-				localStorage.setItem(
-					"refresh_token",
-					refresh_token,
-				);
-			}}
 		></iframe>
 	);
 };
