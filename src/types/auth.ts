6a0f5dea637351647c5fe6244e4a90f59034158b
Floruta Raul <floruta.raul25@gmail.com>
2024-10-14 14:20:02 +0300

    updated the type and added the registerdata type

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,2 +1,15 @@
+export type MsgStatus = { msg: string; status: boolean };
 export type ResetPasswordData = { email: string; lang: "ro" | "en" };
-export type ResetPasswordResponse = { msg: string; status: boolean };
+export type RegisterUserData = {
+	tac: boolean;
+	first_name: string;
+	last_name: string;
+	email: string;
+	phone_number: string;
+	gender: "m" | "f";
+	password: string;
+	birth_date: string;
+	password_confirmation: string;
+	receive_emails: boolean;
+	lang: "ro" | "en";
+};
